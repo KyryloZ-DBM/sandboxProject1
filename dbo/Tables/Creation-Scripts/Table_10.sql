16427b567c0665b661d6439ae5d9f25504f93cea
KyryloZ-DBM <email>
2021-10-04 17:00:36 +0000

    create new tables

---
+++ dbo/Tables/Creation-Scripts/Table_10.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [dbo].[Table_10](	  [hu] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)GO
---
+++ dbo/Tables/Creation-Scripts/Table_14.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [dbo].[Table_14](	  [ojp] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)GO
