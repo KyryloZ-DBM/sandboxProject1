f5707e1b3ee5a32763f0f1d59e2a6abda8050438
KyryloZ-DBM <82807333+KyryloZ-DBM@users.noreply.github.com>
2021-10-05 10:02:11 +0300

    Rename Table_11.sql to Events_11.sql

---
+++ dbo/Tables/Creation-Scripts/Events_11.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_11]
+(
+	  [bhj] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
--- dbo/Tables/Creation-Scripts/Table_11.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE [dbo].[Table_11](	  [bhj] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)GO
