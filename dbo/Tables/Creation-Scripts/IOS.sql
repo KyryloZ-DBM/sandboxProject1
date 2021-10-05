208fdcb0cdf6cb202c1bf256b04989f4a61caddc
KyryloZ-DBM <82807333+KyryloZ-DBM@users.noreply.github.com>
2021-10-05 10:02:28 +0300

    Rename Table_12.sql to IOS.sql

---
+++ dbo/Tables/Creation-Scripts/IOS.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_12]
+(
+	  [yu] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
--- dbo/Tables/Creation-Scripts/Table_12.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE [dbo].[Table_12](	  [yu] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)GO
