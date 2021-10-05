6c2280055598b12d98f5fbfd311ec5ec5f54b036
KyryloZ-DBM <82807333+KyryloZ-DBM@users.noreply.github.com>
2021-10-05 10:01:55 +0300

    Update Table_10.sql

--- dbo/Tables/Creation-Scripts/Table_10.sql
+++ dbo/Tables/Creation-Scripts/Table_10.sql
@@ -1,1 +1,6 @@
-CREATE TABLE [dbo].[Table_10](	  [hu] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)GO
+CREATE TABLE [dbo].[Table_10]
+(
+	  [userName] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
